111f1d58c82d89addc69e4e58d27b00f7e4ba712
Jordy <jordynavalee@gmail.com>
2023-10-25 15:17:16 -0500

    makes category filtering functional

--- olive-said/src/App.js
+++ olive-said/src/App.js
@@ -9,17 +9,28 @@ import Loader from "./Components/Loader";
 export default function App() {
   const [quotes, setQuotes] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(false);
-  // const [currentCategory, setCurrentCategory] = React.useState("all");
+  const [currentCategory, setCurrentCategory] = React.useState("all");
 
   React.useEffect(() => {
     async function getQuotes() {
       setIsLoading(true);
-      const { data: quotes } = await supabase.from("quotes").select("*");
-      setQuotes(quotes);
+
+      let query = supabase.from("quotes").select("*");
+
+      if (currentCategory !== "all")
+        query = query.eq("category", `${currentCategory}`);
+
+      const { data: quotes, error } = await query.limit(100);
+
+      if (!error) setQuotes(quotes);
+      else
+        alert(
+          "Olive can't remember what she said right now. Please try again."
+        );
       setIsLoading(false);
     }
     getQuotes();
-  }, []);
+  }, [currentCategory]);
 
   return (
     <>
